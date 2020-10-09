82700835d9bff8bc26480f97bec832c45c1eb840
Hackerbot1488 <gromovartem04@mail.ru>
2020-10-09 12:39:17 +0300

    changed auth component

--- client/src/components/Auth/Auth.tsx
+++ client/src/components/Auth/Auth.tsx
@@ -9,38 +9,42 @@ export const Auth: React.FC<AuthProps> = ({ onAuth }) => {
 	return (
 		<div className="auth-form">
 			<h1 className="auth-form__header">Awesome chat</h1>
-  <content className="auth-form__content">
-  <label for="name">Type your name</label>
-			<input
-				className="auth-form__input"
-   id="name" 
-				placeholder="Name"
-				value={name}
-				onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-			/>
-  </content>
-  <content className="auth-form__content">
-  <label for="room">Type room number</label>
-			<input
-				className="auth-form__input"
-   id="room" 
-				placeholder="Room"
-				value={room}
-				onChange={(e: ChangeEvent<HTMLInputElement>) => setRoom(e.target.value)}
-				/* onKeyPress={(e: KeyboardEvent<HTMLInputElement>) =>
+			<div className="auth-form__content">
+				<label htmlFor="name">Type your name</label>
+				<input
+					className="auth-form__input"
+					id="name"
+					placeholder="Name"
+					value={name}
+					onChange={(e: ChangeEvent<HTMLInputElement>) =>
+						setName(e.target.value)
+					}
+				/>
+			</div>
+			<div className="auth-form__content">
+				<label htmlFor="room">Type room number</label>
+				<input
+					className="auth-form__input"
+					id="room"
+					placeholder="Room"
+					value={room}
+					onChange={(e: ChangeEvent<HTMLInputElement>) =>
+						setRoom(e.target.value)
+					}
+					/* onKeyPress={(e: KeyboardEvent<HTMLInputElement>) =>
 					(e.key === "Enter" && room.trim() && name.trim()) ??
 					onAuth(room, name)
 				} */
-			/>
-  </content>
-  <content className="auth-form__content">
-			<button
-				className="auth-form__button"
-				onClick={() => room.trim() && name.trim() && onAuth(room, name)}
-			>
-				Join
-			</button>
-  </content>
+				/>
+			</div>
+			<div className="auth-form__content">
+				<button
+					className="auth-form__button"
+					onClick={() => room.trim() && name.trim() && onAuth(room, name)}
+				>
+					Join
+				</button>
+			</div>
 		</div>
 	);
 };
