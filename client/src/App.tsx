d7d2d9dbd93af9f48d96c0ce666e48f8a07a61ab
Hackerbot1488 <gromovartem04@mail.ru>
2020-10-09 21:57:01 +0300

    hz

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.sass";
 import openSocket from "socket.io-client";
 import { Auth } from "./components/Auth/Auth";
+import { Chat } from "./components/Chat/Chat";
 // const socket = openSocket("http://localhost:1488");
 function App() {
 	function auth(port: string, name: string) {
@@ -19,8 +20,8 @@ function App() {
 	}, [socket]);
 	return (
 		<div className="App">
-			{!socket && <Auth onAuth={auth} />}
-			{socket && <h1>Congratulations!</h1>}
+			{socket && <Auth onAuth={auth} />}
+			{!socket && <Chat />}
 		</div>
 	);
 }
