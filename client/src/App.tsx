b1a069e4e67d8963710ecdffaad49de24d37c44b
Hackerbot1488 <gromovartem04@mail.ru>
2020-10-10 21:15:24 +0300

    made main components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,50 @@
 import React, { useEffect, useState } from "react";
 import "./App.sass";
 import openSocket from "socket.io-client";
+import { UserContext } from "./context";
 import { Auth } from "./components/Auth/Auth";
 import { Chat } from "./components/Chat/Chat";
-// const socket = openSocket("http://localhost:1488");
-function App() {
+let uid;
+export const App: React.FC<{}> = () => {
 	function auth(port: string, name: string) {
 		const temp = openSocket(`http://localhost:1488`);
 		console.log(temp);
 		setSocket(temp);
-		temp.emit("new client", { room: port, name });
+		setUsername(name);
+		uid = String(Math.random());
+		setId(uid);
+		configSocket(temp, port, name, uid);
+	}
+	function configSocket(
+		tempSocket: SocketIOClient.Socket,
+		port: string,
+		name: string,
+		id: string
+	) {
+		/* tempSocket.on("get id", ({ idUser }: { idUser: string }) => {
+			console.log(`Set id ${idUser}`);
+			setId(idUser);
+		}); */
+		tempSocket.emit("new client", { room: port, name, id });
 	}
 	const [socket, setSocket] = useState<SocketIOClient.Socket>();
+	const [username, setUsername] = useState("");
+	const [id, setId] = useState("");
 	useEffect(() => {
-		// socket.emit("new client", { msg: "hi" });
 		return () => {
 			socket && socket.disconnect();
 		};
 	}, [socket]);
 	return (
 		<div className="App">
-			{socket && <Auth onAuth={auth} />}
-			{!socket && <Chat />}
+			{!socket && <Auth onAuth={auth} />}
+			{socket && (
+				<UserContext.Provider value={{ socket, id, name: username }}>
+					<Chat />
+				</UserContext.Provider>
+			)}
 		</div>
 	);
-}
+};
 
 export default App;
