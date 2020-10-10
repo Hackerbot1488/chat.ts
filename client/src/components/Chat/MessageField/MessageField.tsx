50c9dff4f1098d147c37d396fbefdb064ae6f950
Hackerbot1488 <gromovartem04@mail.ru>
2020-10-10 12:10:21 +0300

    made message field component

--- client/src/components/Chat/MessageField/MessageField.tsx
+++ client/src/components/Chat/MessageField/MessageField.tsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { KeyboardEvent, useRef, useState } from "react";
 import "./MessageField.sass";
 import { ReactComponent as SendImage } from "../../../images/send.svg";
+
 export const MessageField: React.FC<{}> = () => {
+	const msg = "";
+	const [message, setMessage] = useState("");
+	const ref = useRef<HTMLDivElement>(null);
+	function keyPressHandler(e: KeyboardEvent<HTMLDivElement>) {
+		setTimeout(() => {
+			setMessage(ref.current?.innerText || "");
+			console.log(ref.current?.innerText);
+		}, 0);
+	}
+	function sendMessage() {
+		console.log(message);
+	}
 	return (
 		<div className="message-field">
-			<textarea
-				className="message-field__textarea"
-				placeholder="Type your message..."
-				rows={4}
-			></textarea>
-			<button className="message-field__button">
+			<div
+				className={`message-field__input ${
+					message.trim() ? "" : "message-field__input_empty"
+				}`}
+				contentEditable
+				ref={ref}
+				onKeyPress={(e: KeyboardEvent<HTMLDivElement>) => {
+					if (e.key === "Enter") {
+						e.preventDefault();
+						sendMessage();
+					}
+				}}
+				onKeyDown={(e: KeyboardEvent<HTMLDivElement>) => {
+					e.key !== "Enter" && keyPressHandler(e);
+				}}
+			></div>
+			<button className="message-field__button" onClick={() => sendMessage()}>
 				<SendImage />
 			</button>
 		</div>
