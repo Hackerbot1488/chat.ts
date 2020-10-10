c1e319c1ab492265e8264297ee7135bea2a88b2e
Hackerbot1488 <gromovartem04@mail.ru>
2020-10-10 21:17:13 +0300

    made server

--- server/server.ts
+++ server/server.ts
@@ -31,18 +31,53 @@ io.sockets.on("connection", (socket: UserSocket) => {
 		connections?.splice(connections.indexOf(socket), 1);
 		rooms[socket.room!]?.splice(rooms[socket.room!].indexOf(socket), 1);
 		console.log(`Disconnected: ${connections.length} sockets connected.`);
-		if (rooms[socket.room!].length === 0) {
+		if (rooms[socket.room!]?.length === 0) {
 			delete rooms[socket.room!];
 		}
+		io.sockets.emit("send users", {
+			usersList: rooms[socket.room!]?.map((user) => ({
+				id: user.uid,
+				name: user.name,
+			})),
+		});
+		io.sockets.emit("new msg", {
+			text: `${socket.name} left chat.`,
+			infoId: socket.uid,
+		});
 	});
-	socket.on("new client", (data: { room: string; name: string }) => {
-		socket.name = data.name;
-		socket.room = data.room;
-		socket.id = Date.now().toString();
-		if (socket.room in rooms) {
-			rooms[socket.room].push(socket);
-		} else {
-			rooms[socket.room] = [socket];
+
+	socket.on(
+		"new client",
+		({ room, name, id }: { room: string; name: string; id: string }) => {
+			socket.name = name;
+			socket.room = room;
+			socket.uid = id;
+			if (socket.room in rooms) {
+				rooms[socket.room].push(socket);
+			} else {
+				rooms[socket.room] = [socket];
+			}
+
+			/* socket.emit("get id", { idUser: socket.uid }); */
+			io.sockets.emit("send users", {
+				usersList: rooms[socket.room].map((user) => ({
+					id: user.uid,
+					name: user.name,
+				})),
+			});
+			io.sockets.emit("new msg", {
+				text: `${socket.name} joined to chat.`,
+				infoId: socket.uid,
+			});
 		}
+	);
+
+	socket.on("send msg", ({ message }) => {
+		console.log(message);
+		io.sockets.emit("new msg", {
+			senderId: socket.uid,
+			senderName: socket.name,
+			text: message,
+		});
 	});
 });
